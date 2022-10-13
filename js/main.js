1330dec967077a1cd143af59f87fe7e739cdcd3a
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-10-13 15:54:50 -0500

    🎨 Added some animations

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 const loader = document.querySelector('.loader');
-
+const sideImg = document.getElementById('side-img');
 
 window.addEventListener('load', () => {
     setTimeout(() => {
         loader.classList.add('disable');
     }, 2000);
+
+    setTimeout(() => {
+        sideImg.classList.add('levitation');
+    }, 3300);
 })
